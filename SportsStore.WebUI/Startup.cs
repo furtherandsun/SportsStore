d68f1d5e613dc7358db8e3582051aadd1674070e
gebe <daniel.gradin@outlook.com>
2013-08-29 22:55:45 +0200

    Initial commit

---
+++ SportsStore.WebUI/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace SportsStore.WebUI
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
