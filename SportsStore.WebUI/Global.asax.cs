8912d75d93b070ca8dd4026e3374938a05dfc8cd
gebe <daniel.gradin@outlook.com>
2013-08-29 23:44:47 +0200

    Added and initialized a custom controller factory powered by Ninject

--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SportsStore.WebUI.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -21,6 +22,9 @@ namespace SportsStore.WebUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Sets the current controller factory to one powered by Ninject
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
         }
     }
 }
---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    /// <summary>
+    /// Custom controller factory powered by Ninject.
+    /// </summary>
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+
+        private IKernel NinjectKernel
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public NinjectControllerFactory()
+        {
+            NinjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        /// <summary>
+        /// Add bindings to the Ninject kernel.
+        /// </summary>
+        private void AddBindings()
+        {
+            //throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the appropiate controller object from the Ninject kernel.
+        /// </summary>
+        /// <param name="requestContext"></param>
+        /// <param name="controllerType"></param>
+        /// <returns></returns>
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController) NinjectKernel.Get(controllerType);
+        }
+
+    }
+}
