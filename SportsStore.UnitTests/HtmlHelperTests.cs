4abe6e36d6ddbc691b4e8a4a2e602cae928f76c4
gebe <daniel.gradin@outlook.com>
2013-09-01 00:33:18 +0200

    Added pagination tests

---
+++ SportsStore.UnitTests/HtmlHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.HtmlHelpers;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class HtmlHelperTests
+    {
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            //arrange
+            HtmlHelper helper = null;
+
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                ItemsPerPage = 5,
+                TotalItems = 14
+            };
+
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            //act
+            MvcHtmlString result = helper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            //assert
+            Assert.AreEqual(
+                @"<a href=""Page1"">1</a>" + @"<a class=""selected"" href=""Page2"">2</a>" + @"<a href=""Page3"">3</a>",
+                result.ToString());
+
+        }
+    }
+}
--- SportsStore.UnitTests/ProductControllerTests.cs
+++ SportsStore.UnitTests/ProductControllerTests.cs
@@ -6,16 +6,15 @@ using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
 using System.Collections.Generic;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.UnitTests
 {
     [TestClass]
     public class ProductControllerTests
     {
-        [TestMethod]
-        public void Can_Paginate()
+        private static Mock<IProductRepository> getMockProductRepository()
         {
-            //arrange
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
             mock.Setup(m => m.Products).Returns(new Product[] {
                 new Product {ProductID = 1, Name = "P1"},
@@ -24,20 +23,48 @@ namespace SportsStore.UnitTests
                 new Product {ProductID = 4, Name = "P4"},
                 new Product {ProductID = 5, Name = "P5"}
             }.AsQueryable());
+            return mock;
+        }
+        
+        
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            //arrange
+            Mock<IProductRepository> mock = getMockProductRepository();
 
             ProductController controller = new ProductController(mock.Object);
 
             controller.PageSize = 3; // 3 products per page
 
             //act
-            IEnumerable<Product> result = (IEnumerable<Product>) controller.List(page: 2).Model;
+            ProductListViewModel result = (ProductListViewModel) controller.List(page: 2).Model;
 
             //assert
-            Product[] productsOnPage = result.ToArray();
+            Product[] productsOnPage = result.Products.ToArray();
             Assert.IsTrue(productsOnPage.Length == 2);
             Assert.AreEqual(productsOnPage[0].Name, "P4");
             Assert.AreEqual(productsOnPage[1].Name, "P5");
 
+        }
+
+
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            //arrange
+            Mock<IProductRepository> mock = getMockProductRepository();
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //act
+            ProductListViewModel result = (ProductListViewModel) controller.List(page: 2).Model;
+
+            //assert
+            Assert.AreEqual(2, result.PagingInfo.CurrentPage);
+            Assert.AreEqual(3, result.PagingInfo.ItemsPerPage);
+            Assert.AreEqual(2, result.PagingInfo.TotalPages);
+            Assert.AreEqual(5, result.PagingInfo.TotalItems);
         }
     }
 }
