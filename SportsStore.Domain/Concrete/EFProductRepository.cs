f85d938742f423face02e97ee8715ca61a6f0741
gebe <daniel.gradin@outlook.com>
2013-08-31 16:33:30 +0200

    Implemented a concrete db schema and db context

---
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Concrete
+{
+    /// <summary>
+    /// Concrete implementation of the product repository targeting EF.
+    /// </summary>
+    class EFProductRepository : IProductRepository
+    {
+        EFDbContext context = new EFDbContext();
+
+        /// <summary>
+        /// Retrieve all products.
+        /// </summary>
+        public IQueryable<Entities.Product> Products
+        {
+            get
+            {
+                return context.Products;
+            }
+        }
+    }
+}
