ef56410eb243acf20d3f373c13ef76c6f30c2587
gebe <daniel.gradin@outlook.com>
2013-09-04 01:09:53 +0200

    Added metadata to some of the properties

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace SportsStore.Domain.Entities
 {
     public class Product
     {
+        [HiddenInput(DisplayValue=false)]
         public int ProductID
         {
             get;
@@ -20,6 +23,7 @@ namespace SportsStore.Domain.Entities
             set;
         }
 
+        [DataType(DataType.MultilineText)]
         public string Description
         {
             get;
