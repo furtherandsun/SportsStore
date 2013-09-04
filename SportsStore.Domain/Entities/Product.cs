3c941b5245e86506811166792c48affd5742116d
gebe <daniel.gradin@outlook.com>
2013-09-04 20:04:21 +0200

    Implemented a more rigorous model validation

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -17,12 +17,14 @@ namespace SportsStore.Domain.Entities
             set;
         }
 
+        [Required(ErrorMessage="Please enter a product name")]
         public string Name
         {
             get;
             set;
         }
 
+        [Required(ErrorMessage="Please enter a product description")]
         [DataType(DataType.MultilineText)]
         public string Description
         {
@@ -30,12 +32,15 @@ namespace SportsStore.Domain.Entities
             set;
         }
 
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage="Please enter a positive value")]
         public decimal Price
         {
             get;
             set;
         }
 
+        [Required(ErrorMessage="Please specify a category")]
         public string Category
         {
             get;
